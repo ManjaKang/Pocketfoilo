8e9bd155b9ba2b41be4aa7f96f33f3819bfcf41d
안정현 <kkk0125-@naver.com>
2022-11-22 16:40:18 +0900

    [Fix] pocketsearch follow error & landing마지막 페이지 시작 버튼 추가

--- frontend/pocketfolio/src/components/search/UserSearch.jsx
+++ frontend/pocketfolio/src/components/search/UserSearch.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {unfollowFunc, followFunc} from '../../store/oauthSlice';
 import {getUserInfo} from '../../store/oauthSlice';
@@ -22,13 +22,23 @@ import {
 
 import UserProfile from '../common/UserProfile';
 
-const User = ({userSeq, name, profilePic, describe, followerTotal, followingTotal, hasFollowed, user}) => {
+const User = ({
+  userSeq,
+  name,
+  profilePic,
+  describe,
+  followerTotal,
+  followingTotal,
+  hasFollowed,
+  user,
+}) => {
   const dispatch = useDispatch();
   const [userModal, setUserModal] = useState(false); //프로필 모달 보이게 안보이게
-  const [follow, setFollow] = useState('');
+  const [follow, setFollow] = useState(() => hasFollowed);
+  const flag = useRef();
 
   // 모달 닫는 함수
-  const closeUserModal = (e) => {
+  const closeUserModal = e => {
     setUserModal(false);
   };
 
@@ -39,23 +49,25 @@ const User = ({userSeq, name, profilePic, describe, followerTotal, followingTota
         const {payload} = await dispatch(unfollowFunc(userSeq));
         if (payload) {
           setFollow(false);
+          flag.current = true;
         }
       } else {
         const {payload} = await dispatch(followFunc(userSeq));
         if (payload) {
           setFollow(true);
+          flag.current = true;
         }
       }
     }
-  }
+  };
 
   // 이미지 오류인 경우 기본 이미지 보이게
-  const onErrorImg = (e) => {
-    e.target.src = '/assets/images/logo3.png'
-  }
+  const onErrorImg = e => {
+    e.target.src = '/assets/images/logo3.png';
+  };
 
   // 특정 유저정보 담을 상태
-    const [userInfo, setUserInfo] = useState([]);
+  const [userInfo, setUserInfo] = useState([]);
 
   // 특정 유저정보 조회 함수
   const bringUserInfo = async userSeq => {
@@ -67,21 +79,37 @@ const User = ({userSeq, name, profilePic, describe, followerTotal, followingTota
     getUserInfo();
   }, []);
 
-  return(
+  return (
     <UserItem>
       {/* 팔로우 버튼 */}
       {/* 팔로우 | 로그인한 상태이고, 방 주인이 아닌 경우 가능 */}
       {!user ? <Icon></Icon> : null}
       {user && user.userSeq !== userSeq ? (
         <Icon>
-          <IconDiv className="follow" onClick={e => handleFollow(userSeq, follow)}>
-            {follow||hasFollowed ? <AlreadyFollowIcon /> : <FollowIcon />}
+          <IconDiv
+            className="follow"
+            onClick={e =>
+              handleFollow(
+                userSeq,
+                (flag.current && follow) || (!flag.current && hasFollowed),
+              )
+            }
+          >
+            {(flag.current && follow) || (!flag.current && hasFollowed) ? (
+              <AlreadyFollowIcon />
+            ) : (
+              <FollowIcon />
+            )}
           </IconDiv>
         </Icon>
-      ) : <Icon><IconDiv/></Icon>}
+      ) : (
+        <Icon>
+          <IconDiv />
+        </Icon>
+      )}
       <UserContainer>
         {/* 사용자 프로필 사진 */}
-        <UserImgContainer                   
+        <UserImgContainer
           onClick={e => {
             setUserModal(userSeq);
             bringUserInfo(userSeq);
@@ -92,7 +120,9 @@ const User = ({userSeq, name, profilePic, describe, followerTotal, followingTota
             src={profilePic ? profilePic : '/assets/images/user.png'}
           />
         </UserImgContainer>
-        {userSeq === userModal && <UserProfile userInfo={userInfo} closeUserModal={closeUserModal}/>}
+        {userSeq === userModal && (
+          <UserProfile userInfo={userInfo} closeUserModal={closeUserModal} />
+        )}
         {/* 유저 정보 */}
         <UserInfoContainer>
           <UserNameDiv>{name}</UserNameDiv>
@@ -104,12 +134,12 @@ const User = ({userSeq, name, profilePic, describe, followerTotal, followingTota
         </UserInfoContainer>
       </UserContainer>
     </UserItem>
-  )
-}
+  );
+};
 
 const UserSearch = ({data}) => {
   const user = useSelector(state => state.oauth.user);
-  
+
   useEffect(() => {
     getUserInfo();
   }, []);
@@ -118,9 +148,8 @@ const UserSearch = ({data}) => {
     <>
       <UserCard>
         {data.map(props => {
-          return(
-            <User {...props} user={user}/>)
-          })}
+          return <User {...props} user={user} />;
+        })}
       </UserCard>
     </>
   );
